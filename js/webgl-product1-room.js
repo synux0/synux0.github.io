dc7b84eab0d33256c8f78d2a39760ed3f52e4a27
synux0 <83602384+synux0@users.noreply.github.com>
2023-03-21 02:30:01 +0100

    Finished product1 room

--- js/webgl-product1-room.js
+++ js/webgl-product1-room.js
@@ -1,12 +1,13 @@
 import * as THREE from "three"
-import {OrbitControls} from "OrbitControls"
-import {GLTFLoader} from "GLTFLoader"
+import { OrbitControls } from "OrbitControls"
+import { GLTFLoader } from "GLTFLoader"
 import { AxesHelper } from "three"
 
 //Global elements
 let scene, camera, renderer
 let cameraControls
 let gltfLoader = new GLTFLoader()
+let textureLoader = new THREE.TextureLoader()
 
 init()
 load()
@@ -37,25 +38,26 @@ function init() {
 
     //Camera
     camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 200)
-    camera.position.set(-8, 10, -3);
-    camera.lookAt(new THREE.Vector3(-8, 10, -11))
+    camera.position.set(-8, 10, 13);
+    camera.lookAt(new THREE.Vector3(-8, 10, 5))
     scene.add(camera)
 
     //Camera controls
     cameraControls = new OrbitControls(camera, renderer.domElement)
-    cameraControls.target.set(-8, 10, -11)
+    cameraControls.target.set(-8, 10, 5)
     cameraControls.enabled = true
     cameraControls.enableDamping = true
-    cameraControls.enablePan = true
-    cameraControls.autoRotate = false
+    cameraControls.enablePan = false
+    cameraControls.autoRotate = true
     cameraControls.autoRotateSpeed = 2
+    cameraControls.maxDistance = 12
 }
 
 function load() {
     //SCENE
 
     //Scene Lights
-    let sceneAmbientLight = new THREE.AmbientLight(0xffffff, 0.7)
+    let sceneAmbientLight = new THREE.AmbientLight(0xffffff, 1)
     scene.add(sceneAmbientLight)
 
     //ROOM
@@ -65,7 +67,8 @@ function load() {
     // Floor
     let floorGeometry = new THREE.BoxGeometry(40, 40, 1, 10, 10)
     let floorMaterial = new THREE.MeshStandardMaterial({
-        color: "#A4B5BF"
+        color: "#898581",
+        // map: textureLoader.load("../models/floor/Scene_Root_baseColor.png")
     });
     let floorMesh = new THREE.Mesh(floorGeometry, floorMaterial)
     floorMesh.rotation.x = -Math.PI / 2
@@ -74,17 +77,17 @@ function load() {
     // Roof
     let roofGeometry = new THREE.BoxGeometry(40, 40, 1, 10, 10)
     let roofMaterial = new THREE.MeshStandardMaterial({
-        color: "#A4B5BF"
+        color: "#98908A"
     });
     let roofMesh = new THREE.Mesh(roofGeometry, roofMaterial)
     roofMesh.position.set(0, 25, 0)
     roofMesh.rotation.x = -Math.PI / 2
-    // room.add(roofMesh)
+    room.add(roofMesh)
 
     //Wall1
     let wallGeometry1 = new THREE.BoxGeometry(41, 26, 1, 10, 10)
     let wallMaterial1 = new THREE.MeshStandardMaterial({
-        color: "#A4B5BF"
+        color: "#98908A"
     })
     let wallMesh1 = new THREE.Mesh(wallGeometry1, wallMaterial1)
     wallMesh1.position.set(0, 12.5, -20)
@@ -93,17 +96,17 @@ function load() {
     //Wall2
     let wallGeometry2 = new THREE.BoxGeometry(41, 26, 1, 10, 10)
     let wallMaterial2 = new THREE.MeshStandardMaterial({
-        color: "#A4B5BF"
+        color: "#98908A"
     })
     let wallMesh2 = new THREE.Mesh(wallGeometry2, wallMaterial2)
     wallMesh2.position.set(20, 12.5, 0)
-    wallMesh2.rotation.y = Math.PI/2
+    wallMesh2.rotation.y = Math.PI / 2
     room.add(wallMesh2)
 
     //Wall3
     let wallGeometry3 = new THREE.BoxGeometry(41, 26, 1, 10, 10)
     let wallMaterial3 = new THREE.MeshStandardMaterial({
-        color: "#A4B5BF"
+        color: "#98908A"
     })
     let wallMesh3 = new THREE.Mesh(wallGeometry3, wallMaterial3)
     wallMesh3.position.set(0, 12.5, 20)
@@ -112,12 +115,60 @@ function load() {
     //Wall4
     let wallGeometry4 = new THREE.BoxGeometry(41, 26, 1, 10, 10)
     let wallMaterial4 = new THREE.MeshStandardMaterial({
-        color: "#A4B5BF"
+        color: "#98908A"
     })
     let wallMesh4 = new THREE.Mesh(wallGeometry4, wallMaterial4)
     wallMesh4.position.set(-20, 12.5, 0)
-    wallMesh4.rotation.y = Math.PI/2
+    wallMesh4.rotation.y = Math.PI / 2
     room.add(wallMesh4)
+
+    //Door
+    let door = new THREE.Object3D()
+    gltfLoader.load("../models/door/scene.gltf", function (gltf) {
+        door = gltf.scene
+        door.position.set(-6, 0, -20.2)
+        door.scale.multiplyScalar(10)
+        room.add(door)
+    }, undefined, function (error) {
+        console.error(error)
+    })
+
+    //Empty shelf
+    let emptyShelf = new THREE.Object3D()
+    gltfLoader.load("../models/empty_shelf/scene.gltf", function (gltf) {
+        emptyShelf = gltf.scene
+        emptyShelf.rotation.y = Math.PI / 2
+        emptyShelf.position.set(16, 0, 35)
+        emptyShelf.scale.multiplyScalar(0.15)
+        emptyShelf.add(new AxesHelper(50))
+        room.add(emptyShelf)
+    }, undefined, function (error) {
+        console.error(error)
+    })
+    
+    //Bookshelf
+    let bookshelf = new THREE.Object3D()
+    gltfLoader.load("../models/bookshelf/scene.gltf", function (gltf) {
+        bookshelf = gltf.scene
+        bookshelf.rotation.y = Math.PI
+        bookshelf.position.set(16, 0, -13)
+        bookshelf.scale.multiplyScalar(10)
+        room.add(bookshelf)
+    }, undefined, function (error) {
+        console.error(error)
+    })
+
+    //Window
+    let window = new THREE.Object3D()
+    gltfLoader.load("../models/window/scene.gltf", function (gltf) {
+        window = gltf.scene
+        window.rotation.y = - Math.PI / 2
+        window.position.set(-19.1, 12, 0)
+        window.scale.multiplyScalar(0.1)
+        room.add(window)
+    }, undefined, function (error) {
+        console.error(error)
+    })
 
     //Room lights
     let roomSpotLightWall1 = new THREE.SpotLight(0xffffff, 0.5)
@@ -146,7 +197,7 @@ function load() {
 
     //DESK
     let desk = new THREE.Object3D()
-    desk.position.set(-6, 0, -11)
+    desk.position.set(-6, 0, 5)
     room.add(desk)
 
     //Table
@@ -185,7 +236,7 @@ function load() {
 
     //Desk lights
     let deskSpotLight1 = new THREE.SpotLight(0xffffff, 5)
-    deskSpotLight1.position.set(-2, 15, 0)
+    deskSpotLight1.position.set(-2, 15, 3)
     deskSpotLight1.target = laptop
     deskSpotLight1.penumbra = 1
     deskSpotLight1.distance = 20
@@ -211,7 +262,7 @@ function update() {
 }
 
 function render() {
-	window.requestAnimationFrame(render)
-	update()
-	renderer.render(scene, camera)
+    window.requestAnimationFrame(render)
+    update()
+    renderer.render(scene, camera)
 }
